536b07c3403d4414f5595231a10e7b5890f8f620
SHIVAM ASATI <49972593+shivam019@users.noreply.github.com>
2024-01-09 23:46:41 +0530

    contact page

---
+++ GettingStarted/src/components/Contact.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export const Contact = () => {
+
+
+  return (
+<>
+    <div className='Contact'>
+        <h1> Contact Us</h1>
+    </div>
+</>    
+  )
+}
+
+
+export default Contact;
--- GettingStarted/src/components/Header.jsx
+++ GettingStarted/src/components/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import "./css/Header.css"
 
 const Header = () => {
   return (
     <>
-     <div>Header</div>
+     <div className='navbar'>
 
+
+     <img className='logo' src='https://upload.wikimedia.org/wikipedia/en/thumb/1/12/Swiggy_logo.svg/2560px-Swiggy_logo.svg.png' alt='SwiggyLogo'></img>
+
+     <ul className='nav-links'>
+      <li>Home</li>
+      <li>Resturents</li>
+      <li>Contact</li>
+      <li>Login / Signup</li>
+  </ul>
+</div>
    </>
   )
 }
